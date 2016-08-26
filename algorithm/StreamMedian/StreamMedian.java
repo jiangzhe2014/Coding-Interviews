1540359f33baabaea165a14a03ce1a490c22d80e
会飞的鱼 <jiangzhe2012_love@163.com>
2016-08-26 18:38:17 +0800

    Create StreamMedian.java

---
+++ algorithm/StreamMedian/StreamMedian.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Solution {
+
+    ArrayList<Integer> list = new ArrayList<>();
+    public void Insert(Integer num) {
+    	list.add(num);
+    }
+
+    public Double GetMedian() {
+        Collections.sort(list);
+        if(list.size()%2==0)
+            return (double)(list.get(list.size()/2 -1)+list.get(list.size()/2))/2;
+        else
+            return (double)list.get(list.size()/2);
+    }
+
+
+}
