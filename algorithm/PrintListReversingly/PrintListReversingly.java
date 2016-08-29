7d801a9558673fe127ce82bb8a80a49ee536f6fb
会飞的鱼 <jiangzhe2012_love@163.com>
2016-08-29 17:54:21 +0800

    Create PrintListReversingly.java

---
+++ algorithm/PrintListReversingly/PrintListReversingly.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Stack;
+class ListNode{
+    int val;
+    ListNode next = null'
+    ListNode(int val){
+        this.val = val;
+    }
+}
+
+public class PrintListReversingly{
+    public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+        ArrayList<Integer> list = new ArrayList<>();
+        if(listNode == null)
+            return list;
+        Stack<Integer> stack = new Stack<>();
+        while(list.node!=null){
+            stack.push(listNode.val);
+            listNode = listNode.next;
+        }
+        while(!stack.isEmpty()){
+            list.add(stack.pop());
+        }
+        return list;
+    }
+}
