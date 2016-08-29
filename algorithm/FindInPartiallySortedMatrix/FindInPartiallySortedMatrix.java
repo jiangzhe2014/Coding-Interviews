0f1c55a8d852a6fef8a5d0aa584dd4808c4f964c
会飞的鱼 <jiangzhe2012_love@163.com>
2016-08-29 16:43:09 +0800

    Create FindInPartiallySortedMatrix.java

---
+++ algorithm/FindInPartiallySortedMatrix/FindInPartiallySortedMatrix.java
@@ -0,0 +1,19 @@
+public class FindInPartiallySortedMatrix {
+    public boolean Find(int [][] array,int target) {
+		if(array.length<=0)
+			return false;
+		int colume = array[0].length-1;
+		int row = 0;
+		while(colume>=0 && row<array.length){
+			if(target == array[row][colume])
+				return true;
+			if(target>array[row][colume]){
+				row++;
+			}
+			else if(target < array[row][colume]){
+				colume--;
+			}
+		}
+		return false;
+    }
+}
