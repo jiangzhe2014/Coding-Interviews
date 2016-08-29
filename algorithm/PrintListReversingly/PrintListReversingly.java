718532ae37347b69fe4561ea51650dc8e569d116
会飞的鱼 <jiangzhe2012_love@163.com>
2016-08-29 17:55:05 +0800

    Update PrintListReversingly.java

--- algorithm/PrintListReversingly/PrintListReversingly.java
+++ algorithm/PrintListReversingly/PrintListReversingly.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Stack;
 class ListNode{
     int val;
-    ListNode next = null'
+    ListNode next = null;
     ListNode(int val){
         this.val = val;
     }
