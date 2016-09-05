87bc3a90c37b21d58801d046667a67831314958d
会飞的鱼 <jiangzhe2012_love@163.com>
2016-09-05 21:58:09 +0800

    Create PrintFromToBottom.java

---
+++ algorithm/PrintFromToBottom/PrintFromToBottom.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+ 
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
+
+public class Solution {
+    public ArrayList<Integer> PrintFromTopToBottom(TreeNode root) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        if(root==null)
+            return list;
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        while(!queue.isEmpty()){
+            TreeNode node = queue.remove();
+            list.add(node.val);
+            if(node.left!=null)
+                queue.add(node.left);
+            if(node.right!=null)
+                queue.add(node.right);
+        }
+        return list;       
+    }
+}
